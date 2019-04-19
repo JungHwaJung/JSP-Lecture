55d5d9182bff8bcb746c7dabe8c55dc40ea9f8f2
Junghwa Jung <wjdghk516@naver.com>
2019-04-19 14:51:35 +0900

    Member

--- src/member/MemberProc.java
+++ src/member/MemberProc.java
@@ -21,17 +21,34 @@ public class MemberProc extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		MemberDAO mDao = null;
+		RequestDispatcher rd = null;
 		String action = request.getParameter("action");
 		String strId = request.getParameter("id");
 		System.out.println(action + ", " + strId);
+		
 		switch(action) {
-		case "update":
-			MemberDAO mDao = new MemberDAO();
+		case "update":			//수정버튼
+			mDao = new MemberDAO();
 			MemberDTO member = mDao.selectOne(Integer.parseInt(strId));
 			request.setAttribute("member", member);
-			RequestDispatcher rd = request.getRequestDispatcher("update.jsp");
+			rd = request.getRequestDispatcher("update.jsp");
 			rd.forward(request, response);
+			break;
+			
+		case "delete":			//삭제버튼
+			mDao = new MemberDAO();
+			mDao.deleteMember(Integer.parseInt(strId));
 			mDao.close();
+			//response.sendRedirect("loginMain.jsp");
+			String message = "id = " + strId + " 가 삭제되었습니다.";
+			String url = "loginMain.jsp";
+			request.setAttribute("message", message);
+			request.setAttribute("url", url);
+			rd = request.getRequestDispatcher("alertMsg.jsp");
+			rd.forward(request, response);
+			break;
+			
 		default:
 		}
 	}
